1f14184c3aba83ee6914126502ac2555d96180bd
jolson <yhz20010529@163.com>
2025-07-24 03:46:28 +0000

    modify mysql host

--- config/mysql.js
+++ config/mysql.js
@@ -2,7 +2,7 @@ import mysql from 'mysql2/promise';
 const connection = mysql.createPool({
     host: '119.28.157.86',
     user: 'root',
-    password: '123456!',
+    password: '123456',
     database: 'table_six',
 });
 export default connection;
