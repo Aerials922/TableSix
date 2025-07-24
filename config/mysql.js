fa9067393bd3cfa13a6872249513ca6a8ada45cf
jolson <yhz20010529@163.com>
2025-07-24 02:49:21 +0000

    create mysql connection

---
+++ config/mysql.js
@@ -0,0 +1,8 @@
+import mysql from 'mysql2/promise';
+const connection = mysql.createPool({
+    host: 'localhost',
+    user: 'root',
+    password: 'n3u3da!',
+    database: 'table_six',
+});
+export default connection;
