d6624986907af281bae75c317bb15fe547474437
666zhaobo666 <1606926099@qq.com>
2025-07-30 15:05:26 +0800

    add the method of getting newst data from db

--- service/exchangeService.js
+++ service/exchangeService.js
@@ -4,40 +4,40 @@ export const buyingIn = async (username, ticker, amount) => {
     try {
         const queryToSelectId = 'SELECT id FROM user WHERE username = ?';
         const [rows] = await connection.query(queryToSelectId, [username]);
-        const queryToGetPrice = 'SELECT close FROM '+ticker+'_pricedata ORDER BY timestamp DESC LIMIT 1';
+        const queryToGetPrice = 'SELECT close FROM ' + ticker + '_pricedata ORDER BY timestamp DESC LIMIT 1';
         const [priceRows] = await connection.query(queryToGetPrice, [ticker]);
         if (priceRows.length === 0) {
             console.warn(`Ticker ${ticker} not found.`);
             return res.status(404).json({ message: 'Ticker not found.' });
         }
-        const price = priceRows[0].close; 
+        const price = priceRows[0].close;
         if (rows.length != 0) {
             const userId = rows[0].id;
             const queryToBuyingIn = 'INSERT INTO positions (user_id, ticker, amount, price) VALUES (?, ?, ?, ?) ON DUPLICATE KEY UPDATE amount = amount + VALUES(amount)';
-        const [result] = await connection.query(queryToBuyingIn, [userId, ticker, amount, price]);
-        return {
-            username,
-            ticker,
-            amount,
-            price
-          };        
+            const [result] = await connection.query(queryToBuyingIn, [userId, ticker, amount, price]);
+            return {
+                username,
+                ticker,
+                amount,
+                price
+            };
         } else {
             console.warn(`User ${username} not found.`);
             return res.status(404).json({ message: 'User not found.' });
         }
-        
-    }catch (error) {
+
+    } catch (error) {
         console.error('Error during buying in:', error);
-        
-    }   
+
+    }
 }
 
 export const buyingOut = async (username, ticker, amount) => {
-    
+
     try {
         const queryToSelectId = 'SELECT id FROM user WHERE username = ?';
         const [rows] = await connection.query(queryToSelectId, [username]);
-        const queryToGetPrice = 'SELECT close FROM '+ticker+'_pricedata ORDER BY timestamp DESC LIMIT 1';
+        const queryToGetPrice = 'SELECT close FROM ' + ticker + '_pricedata ORDER BY timestamp DESC LIMIT 1';
         const [priceRows] = await connection.query(queryToGetPrice, [ticker]);
         if (priceRows.length === 0) {
             console.warn(`Ticker ${ticker} not found.`);
@@ -47,7 +47,7 @@ export const buyingOut = async (username, ticker, amount) => {
         if (rows.length != 0) {
             const userId = rows[0].id;
             const queryToBuyingOut = 'UPDATE positions SET amount = amount - ?, price = ?WHERE user_id = ? AND ticker = ?';
-        const [result] = await connection.query(queryToBuyingOut, [amount, price, userId, ticker]);
+            const [result] = await connection.query(queryToBuyingOut, [amount, price, userId, ticker]);
         } else {
             console.warn(`User ${username} not found.`);
             return res.status(404).json({ message: 'User not found.' });
@@ -57,9 +57,9 @@ export const buyingOut = async (username, ticker, amount) => {
             ticker,
             amount,
             price
-          };
+        };
 
-    }    catch (error) {
+    } catch (error) {
         console.error('Error during buying out:', error);
     }
 }
