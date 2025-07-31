b6b095295a1eb4e4781d18d5047436634137c35f
666zhaobo666 <1606926099@qq.com>
2025-07-31 14:42:25 +0800

    add the method to update users cash

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ import financialRoute from './route/financialRoute.js';
 import loginRoute from './route/loginRoute.js';
 import exchangeRoute from './route/exchangeRoute.js';
 import positionRoute from './route/positionRoute.js';
+import userRoute from './route/userRoute.js'; 
 const app = express();
 app.use(cors());
 app.use(express.json());
@@ -14,6 +15,8 @@ app.use('/tab_six/stock', financialRoute);
 app.use('/tab_six/', loginRoute);
 app.use('/tab_six/exchange', exchangeRoute);
 app.use('/tab_six/position', positionRoute);
+app.use('/tab_six/user', userRoute);
+
 const PORT = 3000;
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
